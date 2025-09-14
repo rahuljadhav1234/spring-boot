8a824041e2a3d982ffaf0dabf98324b7002d5c23
Rahul Jadhav <142441621+rahuljadhav1234@users.noreply.github.com>
2025-09-14 17:54:51 +0530

    Add files via upload

---
+++ employees/employees/src/main/java/com/project/springboot/employees/EmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+}
